b4b22220e211cef051979a426f8369426e66abcd
Supriyo <supriyomalstartup@gmail.com>
2025-07-19 14:25:35 +0530

    feat : implementation setup of portfolio class

--- src/Portfolio.cpp
+++ src/Portfolio.cpp
@@ -1,1 +1,9 @@
-// todo : class implementation
+#include "Portfolio.h"
+#include<iostream>
+#include<iomanip> // this is used for formatting.
+using namespace std;
+
+void Portfolio::addInvestment(const string& symbol, int quantity, double price){
+    investments.push_back({symbol, quantity, price, price});
+    cashBalance -= quantity * price;
+}
